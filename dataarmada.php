599199ab26f402592cca4175c25306b5a9357bf6
Daffaizath12 <117889603+Daffaizath12@users.noreply.github.com>
2023-09-19 01:56:57 +0700

    Day 4

--- dataarmada.php
+++ dataarmada.php
@@ -22,7 +22,7 @@ $username = $_SESSION["username"];
 	<!-- My CSS -->
 	<link rel="stylesheet" href="css/style.css">
 
-	<title>AdminHub</title>
+	<title>Petta Tour And Travel</title>
 </head>
 <body>
 
@@ -33,7 +33,7 @@ $username = $_SESSION["username"];
 		<img src="img/logo.png">
 		</a>
 		<ul class="side-menu top">
-			<li class="active">
+			<li>
 				<a href="dashboard.php">
 					<i class='bx bxs-dashboard' ></i>
 					<span class="text">Dashboard</span>
@@ -48,10 +48,10 @@ $username = $_SESSION["username"];
 			<li>
 				<a href="datasupir.php">
 					<i class='bx bxs-group' ></i>
-					<span class="text">Kelola Supir</span>
+					<span class="text">Kelola Sopir</span>
 				</a>
 			</li>
-			<li>
+			<li class="active">
 				<a href="dataarmada.php">
 					<i class='bx bxs-message-dots' ></i>
 					<span class="text">Kelola Armada</span>
@@ -99,131 +99,53 @@ $username = $_SESSION["username"];
 		<main>
 			<div class="head-title">
 				<div class="left">
-					<h1>Dashboard</h1>
-					<ul class="breadcrumb">
-						<li>
-							<a href="#">Dashboard</a>
-						</li>
-						<li><i class='bx bx-chevron-right' ></i></li>
-						<li>
-							<a class="active" href="#">Home</a>
-						</li>
-					</ul>
+					<h1>Kelola Armada</h1>
 				</div>
 			</div>
-
-			<ul class="box-info">
-				<li>
-					<i class='bx bxs-calendar-check' ></i>
-					<span class="text">
-						<h3>1020</h3>
-						<p>New Order</p>
-					</span>
-				</li>
-				<li>
-					<i class='bx bxs-group' ></i>
-					<span class="text">
-						<h3>2834</h3>
-						<p>Customers</p>
-					</span>
-				</li>
-				<li>
-					<i class='bx bxs-dollar-circle' ></i>
-					<span class="text">
-						<h3>Rp. 254.150.000</h3>
-						<p>Total Sales</p>
-					</span>
-				</li>
-			</ul>
-
 
 			<div class="table-data">
 				<div class="order">
 					<div class="head">
-						<h3>Recent Orders</h3>
+						<h3>Data Armada</h3>
 						<i class='bx bx-search' ></i>
 						<i class='bx bx-filter' ></i>
 					</div>
 					<table>
 						<thead>
 							<tr>
-								<th>User</th>
-								<th>Date Order</th>
+								<th>Nama</th>
+								<th>Warna</th>
+                                <th>Jumlah Kursi</th>
 								<th>Status</th>
 							</tr>
 						</thead>
 						<tbody>
 							<tr>
 								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
+									<p>Elep</p>
 								</td>
-								<td>01-10-2021</td>
-								<td><span class="status completed">Completed</span></td>
+                                <td>biru</td>
+								<td>12</td>
+								<td><span class="status completed">Yes</span></td>
 							</tr>
 							<tr>
 								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
+									<p>praoto</p>
 								</td>
-								<td>01-10-2021</td>
-								<td><span class="status pending">Pending</span></td>
+                                <td>silver</td>
+								<td>08</td>
+								<td><span class="status pending">NO</span></td>
 							</tr>
 							<tr>
 								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
+									<p>Inova</p>
 								</td>
-								<td>01-10-2021</td>
-								<td><span class="status process">Process</span></td>
-							</tr>
-							<tr>
-								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
-								</td>
-								<td>01-10-2021</td>
-								<td><span class="status pending">Pending</span></td>
-							</tr>
-							<tr>
-								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
-								</td>
-								<td>01-10-2021</td>
-								<td><span class="status completed">Completed</span></td>
+								<td>putih</td>
+								<td>12</td>
+								<td><span class="status process">On The Way</span></td>
 							</tr>
 						</tbody>
 					</table>
-				</div>
-				<div class="todo">
-					<div class="head">
-						<h3>Todos</h3>
-						<i class='bx bx-plus' ></i>
-						<i class='bx bx-filter' ></i>
-					</div>
-					<ul class="todo-list">
-						<li class="completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-						<li class="completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-						<li class="not-completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-						<li class="completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-						<li class="not-completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-					</ul>
 				</div>
 			</div>
 		</main>
--- datapelanggan.php
+++ datapelanggan.php
@@ -22,7 +22,7 @@ $username = $_SESSION["username"];
 	<!-- My CSS -->
 	<link rel="stylesheet" href="css/style.css">
 
-	<title>AdminHub</title>
+	<title>Petta Tour And Travel</title>
 </head>
 <body>
 
@@ -33,13 +33,13 @@ $username = $_SESSION["username"];
 		<img src="img/logo.png">
 		</a>
 		<ul class="side-menu top">
-			<li class="active">
+			<li>
 				<a href="dashboard.php">
 					<i class='bx bxs-dashboard' ></i>
 					<span class="text">Dashboard</span>
 				</a>
 			</li>
-			<li>
+			<li class="active">
 				<a href="datapelanggan.php">
 					<i class='bx bxs-group' ></i>
 					<span class="text">Kelola Pelanggan</span>
@@ -48,7 +48,7 @@ $username = $_SESSION["username"];
 			<li>
 				<a href="datasupir.php">
 					<i class='bx bxs-group' ></i>
-					<span class="text">Kelola Supir</span>
+					<span class="text">Kelola Sopir</span>
 				</a>
 			</li>
 			<li>
@@ -99,131 +99,65 @@ $username = $_SESSION["username"];
 		<main>
 			<div class="head-title">
 				<div class="left">
-					<h1>Dashboard</h1>
-					<ul class="breadcrumb">
-						<li>
-							<a href="#">Dashboard</a>
-						</li>
-						<li><i class='bx bx-chevron-right' ></i></li>
-						<li>
-							<a class="active" href="#">Home</a>
-						</li>
-					</ul>
+					<h1>Kelola Pelanggan</h1>
 				</div>
 			</div>
-
-			<ul class="box-info">
-				<li>
-					<i class='bx bxs-calendar-check' ></i>
-					<span class="text">
-						<h3>1020</h3>
-						<p>New Order</p>
-					</span>
-				</li>
-				<li>
-					<i class='bx bxs-group' ></i>
-					<span class="text">
-						<h3>2834</h3>
-						<p>Customers</p>
-					</span>
-				</li>
-				<li>
-					<i class='bx bxs-dollar-circle' ></i>
-					<span class="text">
-						<h3>Rp. 254.150.000</h3>
-						<p>Total Sales</p>
-					</span>
-				</li>
-			</ul>
-
 
 			<div class="table-data">
 				<div class="order">
 					<div class="head">
-						<h3>Recent Orders</h3>
+						<h3>Data Pelanggan</h3>
 						<i class='bx bx-search' ></i>
 						<i class='bx bx-filter' ></i>
 					</div>
 					<table>
 						<thead>
 							<tr>
-								<th>User</th>
-								<th>Date Order</th>
-								<th>Status</th>
+								<th>Nama</th>
+								<th>Email</th>
+								<th>Kontak</th>
+								<th>Alamat</th>
 							</tr>
 						</thead>
 						<tbody>
 							<tr>
-								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
-								</td>
-								<td>01-10-2021</td>
-								<td><span class="status completed">Completed</span></td>
+								<td><p>John Doe</p></td>
+								<td>JhonDoe@gmail.com</td>
+								<td>082233344521</td>
+								<td>Gg. Tomat, Gayungan 8, Gayungsari, kec. Kebonsari, Kota Surabaya</td>
 							</tr>
 							<tr>
-								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
-								</td>
-								<td>01-10-2021</td>
-								<td><span class="status pending">Pending</span></td>
+								<td><p>John Doe</p></td>
+								<td>JhonDoe@gmail.com</td>
+								<td>082233344521</td>
+								<td>Gg. Tomat, Gayungan 8, Gayungsari, kec. Kebonsari, Kota Surabaya</td>
 							</tr>
 							<tr>
-								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
-								</td>
-								<td>01-10-2021</td>
-								<td><span class="status process">Process</span></td>
+								<td><p>John Doe</p></td>
+								<td>JhonDoe@gmail.com</td>
+								<td>082233344521</td>
+								<td>Gg. Tomat, Gayungan 8, Gayungsari, kec. Kebonsari, Kota Surabaya</td>
 							</tr>
 							<tr>
-								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
-								</td>
-								<td>01-10-2021</td>
-								<td><span class="status pending">Pending</span></td>
+								<td><p>John Doe</p></td>
+								<td>JhonDoe@gmail.com</td>
+								<td>082233344521</td>
+								<td>Gg. Tomat, Gayungan 8, Gayungsari, kec. Kebonsari, Kota Surabaya</td>
 							</tr>
 							<tr>
-								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
-								</td>
-								<td>01-10-2021</td>
-								<td><span class="status completed">Completed</span></td>
+								<td><p>John Doe</p></td>
+								<td>JhonDoe@gmail.com</td>
+								<td>082233344521</td>
+								<td>Gg. Tomat, Gayungan 8, Gayungsari, kec. Kebonsari, Kota Surabaya</td>
+							</tr>
+							<tr>
+								<td><p>John Doe</p></td>
+								<td>JhonDoe@gmail.com</td>
+								<td>082233344521</td>
+								<td>Gg. Tomat, Gayungan 8, Gayungsari, kec. Kebonsari, Kota Surabaya</td>
 							</tr>
 						</tbody>
 					</table>
-				</div>
-				<div class="todo">
-					<div class="head">
-						<h3>Todos</h3>
-						<i class='bx bx-plus' ></i>
-						<i class='bx bx-filter' ></i>
-					</div>
-					<ul class="todo-list">
-						<li class="completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-						<li class="completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-						<li class="not-completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-						<li class="completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-						<li class="not-completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-					</ul>
 				</div>
 			</div>
 		</main>
